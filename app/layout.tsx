23e0a1e1904a6e5036613dc0bdb847d9c0a4529d
Tacio Jesus <jesustacio@gmail.com>
2026-08-23 12:57:01 -0300

    media: replace hero video

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
           async
         />
         <script
-          src="https://scripts.converteai.net/f7f63c56-fc11-4d6b-889e-71d2f24f657c/players/6a7f903d9b544b5366ba4beb/v4/player.js"
+          src="https://scripts.converteai.net/f7f63c56-fc11-4d6b-889e-71d2f24f657c/players/6a8b16bc2d15b4a5d950c866/v4/player.js"
           async
         />
         <script
--- app/page.tsx
+++ app/page.tsx
@@ -114,7 +114,7 @@ export default function Home() {
               aria-label="Vídeo de apresentação do Método da Memória Musical"
               dangerouslySetInnerHTML={{
                 __html:
-                  '<vturb-smartplayer id="vid-6a7f903d9b544b5366ba4beb" style="display:block;margin:0 auto;width:100%;max-width:400px"><div class="vturb-player-placeholder" style="position:relative;width:100%;padding:177.77777777777777% 0 0;z-index:0;background-color:black"></div></vturb-smartplayer>',
+                  '<vturb-smartplayer id="vid-6a8b16bc2d15b4a5d950c866" style="display:block;margin:0 auto;width:100%;max-width:400px"><div class="vturb-player-placeholder" style="position:relative;width:100%;padding:177.77777777777777% 0 0;z-index:0;background-color:black"></div></vturb-smartplayer>',
               }}
             />
             <p className="lead">
