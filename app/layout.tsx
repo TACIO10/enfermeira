fe2ee95a91c4663e973ed39e87b64c0e53e938bb
Tacio Jesus <jesustacio@gmail.com>
2026-08-30 18:52:12 -0300

    tracking: restore utmify script

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,13 @@ export default function RootLayout({
       <head>
         <style dangerouslySetInnerHTML={{ __html: globalStyles }} />
         <script
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-xcod-sck=""
+          data-utmify-prevent-subids=""
+          async
+          defer
+        />
+        <script
           data-goatcounter="https://enfermeiro.goatcounter.com/count"
           src="https://gc.zgo.at/count.js"
           async
